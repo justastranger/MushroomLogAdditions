c5fc01428290f5405a70c295e9fd5c8c61b48bc5
justastranger <justastrangerlica@gmail.com>
2024-03-29 18:48:38 -0500

    Trim logging and comments for 1.0.0

--- MushroomLogAdditions/ModEntry.cs
+++ MushroomLogAdditions/ModEntry.cs
@@ -90,14 +90,13 @@ namespace MushroomLogAdditions
                 }
             }
 
-            instance.Monitor.Log("Content packs loaded, current additions: ");
-            instance.Monitor.Log(Newtonsoft.Json.JsonConvert.SerializeObject(treeToOutputDict));
+            instance.Monitor.Log("Content packs loaded, current additions:", LogLevel.Trace);
+            instance.Monitor.Log(Newtonsoft.Json.JsonConvert.SerializeObject(treeToOutputDict), LogLevel.Trace);
         }
 
         public static Item OutputMushroomLog(StardewValley.Object machine, Item inputItem, bool probe, MachineItemOutput outputData, out int? overrideMinutesUntilReady)
         {
             overrideMinutesUntilReady = null;
-            instance.Monitor.Log("Mushroom Log Postfix");
 
             // we have to clone the vanilla code since we can't access any of the original method's local variables
             // otherwise this would've been a simple postfix...
@@ -122,7 +121,6 @@ namespace MushroomLogAdditions
                 {
                     string mushroomType = (Game1.random.NextBool(0.05) ? "(O)422" : (Game1.random.NextBool(0.15) ? "(O)420" : "(O)404"));
                     string treeType = tree.treeType.Value;
-                    instance.Monitor.Log($"Testing treeType {treeType}.");
                     if (treeType == "2")
                     {
                         mushroomType = (Game1.random.NextBool(0.1) ? "(O)422" : "(O)420");
@@ -144,8 +142,7 @@ namespace MushroomLogAdditions
                     else if (treeToOutputDict.ContainsKey(treeType))
                     {
                         mushroomType = treeToOutputDict[treeType];
-                        instance.Monitor.Log($"TreeType {treeType} recognized, injecting {mushroomType} into mushroomPossibilities.");
-                    }
+                        }
                     mushroomPossibilities.Add(mushroomType);
                     if (tree.hasMoss.Value)
                     {
@@ -170,105 +167,8 @@ namespace MushroomLogAdditions
                     break;
                 }
             }
-            // confirmed we have an appropriate vanilla mushroomPossibilities, though our added entries are missing
-            // instance.Monitor.Log(Newtonsoft.Json.JsonConvert.SerializeObject(mushroomPossibilities));
             // re-roll the output using the new pool with the old stack amount and quality values
             return ItemRegistry.Create(Game1.random.ChooseFrom(mushroomPossibilities), amount, quality, false);
         }
     }
-
-    //[HarmonyPatch(typeof(StardewValley.Object), nameof(StardewValley.Object.OutputMushroomLog))]
-    //public static class ObjectPatch
-    //{
-    //    [HarmonyPrefix]
-    //    public static bool OutputMushroomLogPrefix(StardewValley.Object machine, Item inputItem, bool probe, MachineItemOutput outputData, out int? overrideMinutesUntilReady, Item __result)
-    //    {
-    //        overrideMinutesUntilReady = null;
-    //        ModEntry.instance.Monitor.Log("Mushroom Log Postfix");
-
-    //        // we have to clone the vanilla code since we can't access any of the original method's local variables
-    //        List<Tree> nearbyTrees = new();
-    //        for (int x = (int)machine.TileLocation.X - 3; x < (int)machine.TileLocation.X + 4; x++)
-    //        {
-    //            for (int y = (int)machine.TileLocation.Y - 3; y < (int)machine.TileLocation.Y + 4; y++)
-    //            {
-    //                Vector2 v = new((float)x, (float)y);
-    //                if (machine.Location.terrainFeatures.ContainsKey(v) && machine.Location.terrainFeatures[v] is Tree tree)
-    //                {
-    //                    nearbyTrees.Add(tree);
-    //                }
-    //            }
-    //        }
-    //        int treeCount = nearbyTrees.Count;
-    //        List<string> mushroomPossibilities = new();
-    //        int mossyCount = 0;
-    //        foreach (Tree tree in nearbyTrees)
-    //        {
-    //            if (tree.growthStage.Value >= 5)
-    //            {
-    //                string mushroomType = (Game1.random.NextBool(0.05) ? "(O)422" : (Game1.random.NextBool(0.15) ? "(O)420" : "(O)404"));
-    //                string treeType = tree.treeType.Value;
-    //                if (!(treeType == "2"))
-    //                {
-    //                    if (!(treeType == "1"))
-    //                    {
-    //                        if (!(treeType == "3"))
-    //                        {
-    //                            if (treeType == "13")
-    //                            {
-    //                                mushroomType = "(O)422";
-    //                            }
-    //                        }
-    //                        else
-    //                        {
-    //                            mushroomType = "(O)281";
-    //                        }
-    //                    }
-    //                    else
-    //                    {
-    //                        mushroomType = "(O)257";
-    //                    }
-    //                }
-    //                else
-    //                {
-    //                    mushroomType = (Game1.random.NextBool(0.1) ? "(O)422" : "(O)420");
-
-    //                    // this small bit here is the only addition to the original function
-    //                    // check to see if the scanned tree is registered as having an output
-    //                    if (ModEntry.treeToOutputDict.ContainsKey(treeType))
-    //                    {
-    //                        mushroomType = ModEntry.treeToOutputDict[treeType];
-    //                    }
-    //                }
-    //                mushroomPossibilities.Add(mushroomType);
-    //                if (tree.hasMoss.Value)
-    //                {
-    //                    mossyCount++;
-    //                }
-    //            }
-    //        }
-
-    //        for (int i = 0; i < Math.Max(1, (int)(nearbyTrees.Count * 0.75f)); i++)
-    //        {
-    //            mushroomPossibilities.Add(Game1.random.NextBool(0.05) ? "(O)422" : (Game1.random.NextBool(0.15) ? "(O)420" : "(O)404"));
-    //        }
-    //        int amount = Math.Max(1, Math.Min(5, Game1.random.Next(1, 3) * (nearbyTrees.Count / 2)));
-    //        int quality = 0;
-    //        float qualityBoostChance = mossyCount * 0.025f + treeCount * 0.025f;
-    //        while (Game1.random.NextDouble() < (double)qualityBoostChance)
-    //        {
-    //            quality++;
-    //            if (quality == 3)
-    //            {
-    //                quality = 4;
-    //                break;
-    //            }
-    //        }
-
-    //        ModEntry.instance.Monitor.Log(Newtonsoft.Json.JsonConvert.SerializeObject(mushroomPossibilities));
-    //        // re-roll the output using the new pool with the old stack amount and quality values
-    //        __result = ItemRegistry.Create(Game1.random.ChooseFrom(mushroomPossibilities), amount, quality, false);
-    //        return false;
-    //    }
-    //}
 }
